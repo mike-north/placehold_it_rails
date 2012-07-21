79f19daceab33e688b53b6aa2588ffc00bdc51ac
Michael North <michael.north@truenorthapps.com>
2012-07-21 18:58:06 -0400

    helpers included in mongoid document

--- lib/placehold_it.rb
+++ lib/placehold_it.rb
@@ -1,10 +1,10 @@
-module PlaceholdIt
-end
-
 require "placehold_it/engine"
-
+require "placehold_it/common"
 require "placehold_it/view_extensions"
 require "placehold_it/controller_extensions"
 
-ActionView::Base.send :include, PlaceholdIt::ViewExtensions
-ActionController::Base.send :include, PlaceholdIt::ControllerExtensions
+module PlaceholdIt
+  ActionView::Base.send :include, PlaceholdIt::ViewExtensions
+  ActionController::Base.send :include, PlaceholdIt::ControllerExtensions
+  ActiveModel::Naming.send :include, PlaceholdIt::Common
+end
